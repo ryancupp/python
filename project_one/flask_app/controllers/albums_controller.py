dc109403972bddae45824479f3321adcfa2ea25c
ryancupp <ryancupp89@gmail.com>
2022-12-07 16:14:40 -0700

    latest commit

--- project_one/flask_app/controllers/albums_controller.py
+++ project_one/flask_app/controllers/albums_controller.py
@@ -3,11 +3,16 @@ from flask import render_template, redirect, request, flash, session
 from flask_app.models.users_model import User
 from flask_app.models.albums_model import Album
 
+
 @app.route('/albums/new')
 def new_album_form():
     if 'user_id' not in session:
-        return redirect ('/')
-    return render_template("new_album.html")
+        return redirect('/')
+    user_data = {
+        'id': session['user_id']
+    }
+    logged_user = User.get_by_id(user_data)
+    return render_template("new_album.html", logged_user=logged_user)
 
 @app.route('/albums/create', methods=['POST'])
 def process_album():
@@ -20,4 +25,53 @@ def process_album():
         'user_id': session['user_id']
     }
     Album.create(data)
+    return redirect('/welcome')
+
+@app.route("/albums/<int:id>")
+def show_album(id):
+    album = Album.get_by_id({'id':id})
+    user_data = {
+        'id': session['user_id']
+    }
+    logged_user = User.get_by_id(user_data)
+    return render_template("show_album.html", album = album, logged_user = logged_user)    
+
+@app.route("/albums/<int:id>/edit")
+def edit_album_form(id):
+    if 'user_id' not in session:
+        return redirect('/')
+    album = Album.get_by_id({'id':id})
+    if not int(session['user_id']) == album.user_id:
+        return redirect('/welcome')
+    album = Album.get_by_id({'id':id})
+    user_data = {
+        'id': session['user_id']
+    }
+    logged_user = User.get_by_id(user_data)
+    return render_template("edit_album.html", album = album, logged_user = logged_user)
+
+@app.route("/albums/<int:id>/update", methods=['POST'])
+def update_album(id):
+    if 'user_id' not in session:
+        return redirect('/')
+    album = Album.get_by_id({'id':id})
+    if not int(session['user_id']) == album.user_id:
+        return redirect('/welcome')
+    if not Album.validator(request.form):
+        return redirect(f"/albums/{id}/edit")
+    data = {
+        **request.form,
+        'id' : id
+    }
+    Album.update(data)
+    return redirect('/welcome')
+
+@app.route("/albums/<int:id>/delete")
+def delete_album(id):
+    if 'user_id' not in session:
+        return redirect('/')
+    album = Album.get_by_id({'id':id})
+    if not int(session['user_id']) == album.user_id:
+        return redirect ('/welcome')
+    Album.delete({'id':id})
     return redirect('/welcome')
--- project_one/flask_app/models/albums_model.py
+++ project_one/flask_app/models/albums_model.py
@@ -2,8 +2,9 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 from flask_app.models import users_model
 
+
 class Album:
-    def __init__(self,data):
+    def __init__(self, data):
         self.id = data['id']
         self.album_title = data['album_title']
         self.artist = data['artist']
@@ -13,14 +14,14 @@ class Album:
         self.updated_at = data['updated_at']
         self.user_id = data['user_id']
 
-    @classmethod 
-    def create(cls,data):
+    @classmethod
+    def create(cls, data):
         query = "INSERT INTO albums (album_title, artist, genre, favorite_song, user_id) VALUES (%(album_title)s, %(artist)s, %(genre)s, %(favorite_song)s, %(user_id)s);"
-        return connectToMySQL('spotify').query_db(query,data)
+        return connectToMySQL('spotify').query_db(query, data)
 
     @classmethod
     def get_all(cls):
-        query = "SELECT * FROM albums JOIN users on albums.user_id = users.id;" 
+        query = "SELECT * FROM albums JOIN users on albums.user_id = users.id;"
         results = connectToMySQL('spotify').query_db(query)
         if len(results) > 0:
             all_albums = []
@@ -28,7 +29,7 @@ class Album:
                 this_album = cls(row)
                 user_data = {
                     **row,
-                    'id' : row['users.id'],
+                    'id': row['users.id'],
                     'created_at': row['users.created_at'],
                     'updated_at': row['users.updated_at']
                 }
@@ -37,6 +38,34 @@ class Album:
                 all_albums.append(this_album)
             return all_albums
         return []
+
+    @classmethod
+    def get_by_id(cls, data):
+        query = "SELECT * FROM albums JOIN users on users.id = albums.user_id WHERE albums.id = %(id)s;"
+        results = connectToMySQL('spotify').query_db(query, data)
+        if len(results) < 1:
+            return False
+        row = results[0]
+        this_album = cls(row)
+        user_data = {
+            **row,
+            'id': row['users.id'],
+            'created_at': row['users.created_at'],
+            'updated_at': row['users.updated_at']
+        }
+        poster = users_model.User(user_data)
+        this_album.poster = poster
+        return this_album
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE albums SET album_title = %(album_title)s, artist = %(artist)s, genre = %(genre)s, favorite_song = %(favorite_song)s WHERE id = %(id)s;"
+        return connectToMySQL('spotify').query_db(query, data)
+
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE from albums where ID = %(id)s;"
+        return connectToMySQL('spotify').query_db(query, data)
 
     @staticmethod
     def validator(form_data):
